afd86fd2f9c60de5da684f8142b390b93f2bf04d
DouaeChmihi <douaeochmihi@gmail.com>
2024-11-27 11:35:25 +0100

    first commit

---
+++ src/main/java/com/example/lab_github_gates_douae_chmihi/LabGithubGatesDouaeChmihiApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_github_gates_douae_chmihi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGithubGatesDouaeChmihiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabGithubGatesDouaeChmihiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab_github_gates_douae_chmihi/LabGithubGatesDouaeChmihiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_github_gates_douae_chmihi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabGithubGatesDouaeChmihiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
